d303735ff7353926596e5591766c20fab23e07d2
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-21 18:12:21 +0530

    crate session factory

--- src/main/java/lk/ijse/thogakadejakartaeebackend/config/SessionFactoryConfig.java
+++ src/main/java/lk/ijse/thogakadejakartaeebackend/config/SessionFactoryConfig.java
@@ -1,4 +1,29 @@
 package lk.ijse.thogakadejakartaeebackend.config;
 
+import lk.ijse.thogakadejakartaeebackend.entities.Customers;
+import lk.ijse.thogakadejakartaeebackend.entities.Item;
+import lk.ijse.thogakadejakartaeebackend.entities.Order;
+import lk.ijse.thogakadejakartaeebackend.entities.OrderDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class SessionFactoryConfig {
+    private static SessionFactoryConfig sessionFactoryConfig;
+    private final SessionFactory sessionFactory;
+    private SessionFactoryConfig() {
+        sessionFactory = new Configuration()
+                .configure()
+                .addAnnotatedClass(Customers.class)
+                .addAnnotatedClass(Item.class)
+                .addAnnotatedClass(Order.class)
+                .addAnnotatedClass(OrderDetail.class)
+                .buildSessionFactory();
+    }
+    public static SessionFactoryConfig getInstance(){
+        return (sessionFactoryConfig == null) ? sessionFactoryConfig = new SessionFactoryConfig() : sessionFactoryConfig;
+    }
+    public Session getSessionFactory(){
+        return sessionFactory.openSession();
+    }
 }
