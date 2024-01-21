7c23dba27d87fc4802658e79c4b666d433af9cc1
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-21 18:18:02 +0530

    change SessionFactoryConfig

--- src/main/java/lk/ijse/thogakadejakartaeebackend/config/SessionFactoryConfig.java
+++ src/main/java/lk/ijse/thogakadejakartaeebackend/config/SessionFactoryConfig.java
@@ -8,17 +8,27 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class SessionFactoryConfig {
     private static SessionFactoryConfig sessionFactoryConfig;
     private final SessionFactory sessionFactory;
     private SessionFactoryConfig() {
-        sessionFactory = new Configuration()
-                .configure()
+        Configuration configuration = new Configuration();
+        Properties properties = new Properties();
+
+        try {
+            properties.load(ClassLoader.getSystemClassLoader().getResourceAsStream("hibernate.Properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        configuration
                 .addAnnotatedClass(Customers.class)
                 .addAnnotatedClass(Item.class)
                 .addAnnotatedClass(Order.class)
-                .addAnnotatedClass(OrderDetail.class)
-                .buildSessionFactory();
+                .addAnnotatedClass(OrderDetail.class);
+        sessionFactory=configuration.setProperties(properties).buildSessionFactory();
     }
     public static SessionFactoryConfig getInstance(){
         return (sessionFactoryConfig == null) ? sessionFactoryConfig = new SessionFactoryConfig() : sessionFactoryConfig;
