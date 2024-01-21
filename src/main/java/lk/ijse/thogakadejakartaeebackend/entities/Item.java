1ebe778d854eaf7f68e8a9a6df5ec2dd9045c826
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-21 17:52:47 +0530

    add item entity bean

--- src/main/java/lk/ijse/thogakadejakartaeebackend/entities/Item.java
+++ src/main/java/lk/ijse/thogakadejakartaeebackend/entities/Item.java
@@ -1,4 +1,27 @@
 package lk.ijse.thogakadejakartaeebackend.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "item")
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private int id;
+    @Column(name = "item_name")
+    private String itemName;
+    @Column(name = "item_quantity")
+    private int qty;
+    @Column(name = "unit_price")
+    private double unitPrice;
+
+//    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy =  "item")
+//    private List<OrderDetail> orderDetails = new ArrayList<>();
 }
