f0d6bc909875a7a6a62b5a18d36ccf24cac85ab1
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-21 18:03:29 +0530

    add OrderDetail entity bean

--- src/main/java/lk/ijse/thogakadejakartaeebackend/entities/OrderDetail.java
+++ src/main/java/lk/ijse/thogakadejakartaeebackend/entities/OrderDetail.java
@@ -1,4 +1,33 @@
 package lk.ijse.thogakadejakartaeebackend.entities;
 
+import jakarta.persistence.*;
+import lk.ijse.thogakadejakartaeebackend.embedded.OrderDetailPK;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "order_detail")
 public class OrderDetail {
+    @EmbeddedId
+    private OrderDetailPK orderDetailPK;
+    @Column(name = "order_quantity")
+    private int qty;
+    @Column(name = "order_price")
+    private double price;
+    @ManyToOne
+    @JoinColumn(name = "order_id",
+            referencedColumnName = "order_id",
+            insertable = false,
+            updatable = false)
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "item_id",
+            referencedColumnName = "item_id",
+            insertable = false,
+            updatable = false)
+    private Item item;
 }
