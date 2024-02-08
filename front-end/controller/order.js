fe300dc197f6f185c3edd6fddaad619ce89c4ea1
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-09 02:59:59 +0530

    add btn done with total txt

--- front-end/controller/order.js
+++ front-end/controller/order.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
     setItmIds();
     setItmCmbAction();
 });
+
+let totalAll = 0;
 function getCusIds() {
     $('#cus-id-slt-bx').empty();
     $.ajax({
@@ -40,7 +42,7 @@ function setItmIds(){
         success: function (resp) {
             console.log("Success: ", resp);
             for (const item of resp) {
-                let optionValue = `${item.description},${item.qtyOnHand},${item.unitPrice}`;
+                let optionValue = `${item.code},${item.description},${item.qtyOnHand},${item.unitPrice}`;
                 $('#itm-id-slt-bx').append(`<option value=${optionValue}>${item.code}</option>`);
             }
         }
@@ -50,9 +52,9 @@ function setItmCmbAction(){
     $('#itm-id-slt-bx').on('change', function() {
         let selectedValue = $(this).val();
         let optionValues = selectedValue.split(',');
-        let description = optionValues[0];
-        let qtyOnHand = optionValues[1];
-        let unitPrice = optionValues[2];
+        let description = optionValues[1];
+        let qtyOnHand = optionValues[2];
+        let unitPrice = optionValues[3];
 
         $('#itm-des-or-txt').val(description);
         $('#qoh-or-txt').val(qtyOnHand);
@@ -60,17 +62,17 @@ function setItmCmbAction(){
     });
 }
 $('#add-btn').click(function () {
-    let code = $('#itm-id-slt-bx').val();
-    let description = $('#itm-des-or-txt').val();
+    let selectedValue = $('#itm-id-slt-bx').val();
+    let selectedValues = selectedValue.split(',');
+    let code = selectedValues[0];
+    let description = selectedValues[1];
     let qty = $('#qty-or-txt').val();
     let unitPrice = $('#prz-or-txt').val();
     let total = qty * unitPrice;
 
-    console.log(code);
-    console.log(description);
-    console.log(qty);
-    console.log(unitPrice);
-    console.log(total);
+    totalAll += total;
+
+    $('#fl-or-ttl-txt').text(totalAll);
 
     $('#order-tbl').append(`<tr>
         <td>${code}</td>
